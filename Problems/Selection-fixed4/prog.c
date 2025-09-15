0b36d9d135df97a413133efb3c635157e83b0656
552266 <rodr0244@gpel8.cs.nor.ou.edu>
2025-09-15 16:59:52 -0500

    Added Selection Sort and Input

--- Problems/Selection-fixed4/prog.c
+++ Problems/Selection-fixed4/prog.c
@@ -1,3 +1,4 @@
+
 /*
   This is a small program to demonstrate gcov. It
   can take no arguments or 1 argument. It prints
@@ -9,6 +10,9 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+
+void selectionSort(int arr[], int n);
+void swap(int *x, int *y);
 
 int main(int argc, char * argv[]) {
   // No arguments passed
@@ -29,5 +33,68 @@ int main(int argc, char * argv[]) {
   // more than 1 argument
   else
     printf("I need less arguments!\n");
+
+  
+  FILE *input = fopen(argv[1], "r"); // opens the input file based on first execution argument with read permission.
+  if (input == NULL)
+  { // error handling for file opening. This may be removed to check for coverage.
+    printf("Error opening input file.\n");
+    return 1;
+  }
+
+  int n; // Variable to hold the size of the array.
+
+  // Reads the first line of the input file to determine the size of the array.
+  // It returns a 1 if successful and assigns the to n based of the memory address of n.
+  // It also checks to make that the allocation would be valid given the context.
+  // An easy example is if the user enters a negative number which would be accepted by %d but would be invalid for an array size.
+  if (fscanf(input, "%d", &n) != 1 || n <= 0)
+  {
+    printf("Invalid array size in input file.\n");
+    fclose(input);
+    return 1;
+  }
+
+  float *arr = malloc(n * sizeof(float));
+  // Removed arr == null to check for coverage.
+  // An invalid n value would cause malloc to return null but this is allready checked for above.
+
+  // Write the array elements from the file into the array.
+  // fscan picks up where it left off skipping the int determining the size of the array.
+  for (int i = 0; i < n; i++)
+  {
+    fscanf(input, "%f", &arr[i]);
+  }
+
+
+  //Selection sort function call.
+  //Sorting algorithm documentation link is above.
+  selectionSort(arr,n);
+
+
+  free(arr); // Free the allocated memory for the array.
+  fclose(input); // Close the input file.  
   return 0;
+}
+
+
+void selectionSort(int arr[], int n) {
+    int min;
+    for (int i = 0; i < n - 1; i++) {
+        min = arr[i];
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        
+    
+        swap(&arr[min], &arr[i]);
+    }
+}
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
